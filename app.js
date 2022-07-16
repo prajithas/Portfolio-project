45ed491aac4120fe85ffd314f2ada410551336a1
Prajitha <98685133+prajithas@users.noreply.github.com>
2022-07-16 18:22:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = new express();
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static('/dist/frontend'));
+app.use(express.static('./dist/frontend'));
 app.get('*', function(req, res) {
   res.sendFile('index.html');
  });
