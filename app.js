5c0e846b4a87202bfce05b9259be565e4d48116c
Prajitha <98685133+prajithas@users.noreply.github.com>
2022-07-18 11:31:37 +0530

    u

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const express= require('express');
 const cors = require('cors');
 const TestimonialData = require('./src/model/Testimonialdata');
 const path = require('path');
-
 var app = new express();
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static('./dist/frontend'));
-app.get('*', function(req, res) {
-  res.sendFile('index.html');
- });
+app.use(express.static('./dist/frontend>'));
 
 // Backend root
 app.get('/api/', (req, res) => {
@@ -24,7 +20,10 @@ app.get('/api/testimonials',function(req,res){
                   res.send(testimonial);
               });
 });
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
+ });
 // Connecting with server
-app.listen(process.env.PORT || 3000, function(){
+app.listen(3000, function(){
     console.log('listening to port 3000');
 });
